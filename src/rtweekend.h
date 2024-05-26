f8536b852c136a7b0a054b17d29b6374843ccbeb
Ben Gavan <BenGavan>
2024-05-26 17:43:55 +0100

    Introduce world and rtweekend header

---
+++ src/rtweekend.h
@@ -0,0 +1,26 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// usings
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+//consts
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897931159979634685;
+
+// utility funcs
+inline double degree_to_radians(double deg) {
+    return deg * pi / 180.0;
+}
+
+// common headers
+#include "ray.h"
+#include "vec3.h"
+
+#endif
