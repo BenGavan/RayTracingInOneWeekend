2fd9454a95efa3246ba24052016b3abe0187b08d
Ben Gavan <BenGavan>
2024-05-31 01:20:50 +0100

    Add scatter depth limit (the maximum number of times a ray can be scattered before it is considered to be black.  This was added to prevent the stack exploding from a deep recursion depth.)

--- src/camera.h
+++ src/camera.h
@@ -15,9 +15,10 @@ Will be responsible for:
 
 class camera{
 public:
-    double aspect_ratio = 10;     // Ratio of image width over height
-    int    image_width  = 100;    // Rendered image width in pixels
-    int    samples_per_pixel = 10;
+    double aspect_ratio      = 10;   // Ratio of image width over height
+    int    image_width       = 100;  // Rendered image width in pixels
+    int    samples_per_pixel = 10;   // Number of rays (samples) sent out per pixel
+    int    max_depth         = 10;   // Max number of times a single ray can bounce within the scene 
 
     void render(const hittable &world) {
         initialize();
@@ -32,7 +33,7 @@ public:
                 color pixel_color(0,0,0);
                 for (int sample = 0; sample < samples_per_pixel; ++sample) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_color, samples_per_pixel);
             }
@@ -89,11 +90,16 @@ private:
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
-    color ray_color(const ray &r, const hittable &world) const {
+    color ray_color(const ray &r, int depth_remaining, const hittable &world) const {
         hit_record rec;
+
+        if (depth_remaining <= 0) {
+            return color(0,0,0);  // this ray has been scattered so many times, it can be considered to be black (limit inplave to stop stack blowing up)
+        }
+
         if (world.hit(r, interval(0, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.9 * ray_color(ray(rec.p, direction), world);
+            return 0.9 * ray_color(ray(rec.p, direction), depth_remaining-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
