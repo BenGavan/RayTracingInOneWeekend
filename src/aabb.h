9a69258db67f0d42c6a1126d53579e68ada556e5
Ben Gavan <BenGavan>
2024-07-23 20:04:22 +0100

    Change to an optimised AABB hit method

--- src/aabb.h
+++ src/aabb.h
@@ -26,14 +26,19 @@ public:
 
     bool hit(const ray& r, interval ray_t) const {
         for (int a = 0; a < 3; a++) {
-            double t0 = fmin((axis(a).min - r.origin()[a]) / r.direction()[a], 
-                             (axis(a).max - r.origin()[a]) / r.direction()[a]);
+            double invD = 1 / r.direction()[a];
+            double orig = r.origin()[a];
 
-            double t1 = fmax((axis(a).min - r.origin()[a]) / r.direction()[a], 
-                             (axis(a).max - r.origin()[a]) / r.direction()[a]);
+            double t0 = (axis(a).min - orig) * invD;
+            double t1 = (axis(a).max - orig) * invD;
 
-            ray_t.min = fmax(t0, ray_t.min);
-            ray_t.max = fmax(t0, ray_t.max);
+            if (invD < 0) {
+                std::swap(t0, t1);
+            }
+
+            if (t0 > ray_t.min) ray_t.min = t0;
+            if (t1 < ray_t.max) ray_t.max = t1;
+
             if (ray_t.max <= ray_t.min) {
                 return false;
             }
