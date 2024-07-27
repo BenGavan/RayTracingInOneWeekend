20e3e813608220d2fc7e2eb8058162191a50e183
Ben Gavan <BenGavan>
2024-07-27 10:43:55 +0100

    Add scale to perlin noise to control the spatial frequency

--- src/texture.h
+++ src/texture.h
@@ -80,12 +80,15 @@ class noise_texture : public texture {
 public:
     noise_texture() {}
 
+    noise_texture(double _scale) : scale(_scale) {}
+
     color value(double u, double v, const point3& p) const override {
-        return color(1,1,1) * noise.noise(p);
+        return color(1,1,1) * noise.noise(scale * p);
     }
 
 private:
     perlin noise;
+    double scale;
 };
 
 #endif
